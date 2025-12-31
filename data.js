7d833cad681cd1df3362325326c3480a4a27f0ae
MAUNKEY01 <vyasmaun15@outlook.com>
2025-12-31 12:14:33 +0000

    Update data.js

--- data.js
+++ data.js
@@ -1,1 +1,14 @@
-
+window.TRACKS = [
+  {
+    title: "Phonk Killer",
+    videoId: "6x7B0GmZpVY"
+  },
+  {
+    title: "Midnight Drift",
+    videoId: "VW3t6n6Y0JQ"
+  },
+  {
+    title: "Memphis Doom",
+    videoId: "6fRUpN0pFHM"
+  }
+];
