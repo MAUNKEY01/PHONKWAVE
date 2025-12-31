f5a07b8de558f98b54ad1e38cb87374d0bc31384
MAUNKEY01 <vyasmaun15@outlook.com>
2025-12-31 12:17:14 +0000

    Update discover.js

--- discover.js
+++ discover.js
@@ -1,1 +1,22 @@
+let current = 0;
+let likes = JSON.parse(localStorage.getItem("likes")) || [];
 
+function showTrack() {
+  let track = TRACKS[current];
+  loadVideo(track.videoId);
+}
+
+document.getElementById("like").onclick = () => {
+  likes.push(TRACKS[current]);
+  localStorage.setItem("likes", JSON.stringify(likes));
+  next();
+};
+
+document.getElementById("nope").onclick = next;
+
+function next() {
+  current = (current + 1) % TRACKS.length;
+  showTrack();
+}
+
+showTrack();
