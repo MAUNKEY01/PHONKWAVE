a365047f5289c0a38c6442c5dedf1364f859aed1
MAUNKEY01 <vyasmaun15@outlook.com>
2025-12-31 13:43:38 +0000

    Create data.js

---
+++ data.js
@@ -0,0 +1,20 @@
+const PHONKS = [
+  {
+    title: "METAMORPHOSIS",
+    artist: "INTERWORLD",
+    videoId: "6LxY7kL5rVg",
+    cover: "https://i.ytimg.com/vi/6LxY7kL5rVg/hqdefault.jpg"
+  },
+  {
+    title: "MURDER IN MY MIND",
+    artist: "KORDHELL",
+    videoId: "w-sQRS-Lc9k",
+    cover: "https://i.ytimg.com/vi/w-sQRS-Lc9k/hqdefault.jpg"
+  },
+  {
+    title: "NEON BLADE",
+    artist: "MOONDEITY",
+    videoId: "7J5b4O9vF8k",
+    cover: "https://i.ytimg.com/vi/7J5b4O9vF8k/hqdefault.jpg"
+  }
+];
